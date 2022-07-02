0c18634c00db2e988ef014e9893f4400169fb887
kingsor <avventureweb@gmail.com>
2022-07-02 19:33:51 +0200

    Update JsonSerializerOptions

--- NeetpiqFuncApp/ParseUrl.cs
+++ NeetpiqFuncApp/ParseUrl.cs
@@ -2,7 +2,9 @@
 using Microsoft.Azure.Functions.Worker.Http;
 using Microsoft.Extensions.Logging;
 using System.Net;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Web;
 
 namespace NeetpiqFuncApp
@@ -16,30 +18,35 @@ namespace NeetpiqFuncApp
             _logger = loggerFactory.CreateLogger<ParseUrl>();
         }
 
+        // public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "smartreader/{url:alpha}")] HttpRequestData req, string url)
         [Function("ParseUrl")]
-        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequestData req)
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "SmartReader/ParseUrl")] HttpRequestData req)
         {
             _logger.LogInformation("C# HTTP trigger function processed a request.");
 
             string responseMessage = "This HTTP triggered function executed successfully. Pass a URL in the query string for a personalized response.";
 
-            string url = string.Empty;
+            string urlParam = string.Empty;
             var query = HttpUtility.ParseQueryString(req.Url.Query);
             
             if(query != null && query.Count >= 1)
             {
-                url = query["url"];
+                urlParam = query["url"] ?? string.Empty;
+                _logger.LogInformation($"Parameter [url]=[{urlParam}]");
             }
 
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            if (!string.IsNullOrEmpty(url))
+            if (!string.IsNullOrEmpty(urlParam))
             {
-                SmartReader.Article article = await SmartReader.Reader.ParseArticleAsync(url);
-                var options = new JsonSerializerOptions() { WriteIndented = true };
+                SmartReader.Article article = await SmartReader.Reader.ParseArticleAsync(urlParam);
+                var options = new JsonSerializerOptions()
+                {
+                    Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping,
+                    WriteIndented = true
+                };
                 responseMessage = JsonSerializer.Serialize<SmartReader.Article>(article, options);
-                //await response.WriteAsJsonAsync(responseMessage);
             }
 
             await response.WriteStringAsync(responseMessage);
